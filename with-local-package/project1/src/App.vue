f8622380f3c0796cb1d3d19907e18c587bb9f84d
Artur Y <artawower33@gmail.com>
2021-03-14 23:20:29 +0300

    Init sample

---
+++ with-local-package/project1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg="Our shared component" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { HelloWorld } from "shared";
+
+ import 'shared/dist/shared.css';
+
+@Options({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {}
+</script>
+
---
+++ with-local-package/shared/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="awesome-title">{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    msg: String
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.awesome-title {
+  color: purple;
+}
+</style>
